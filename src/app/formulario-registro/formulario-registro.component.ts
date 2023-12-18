121718fc99bcde638e1169e231f09f13c22ee4ff
eduar <eduardojosue.cerrato@institutvidreres.cat>
2023-12-18 09:44:22 +0100

    Todo a Angular

---
+++ src/app/formulario-registro/formulario-registro.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
+
+@Component({
+  selector: 'app-formulario-registro',
+  standalone: true,
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './formulario-registro.component.html',
+  styleUrl: './formulario-registro.component.css'
+})
+export class FormularioRegistroComponent {
+
+}
