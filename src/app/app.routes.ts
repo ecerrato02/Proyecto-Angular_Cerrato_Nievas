3970609ce8590385118c6f25f03ab006344cab6c
EDUNI11 <edunievasdris@gmail.com>
2023-12-19 11:21:25 +0100

    Revert "Todo a Angular"
    This reverts commit 121718fc99bcde638e1169e231f09f13c22ee4ff.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,11 @@ import { CatalogoComponent } from './catalogo/catalogo.component';
 import {CondicionesComponent} from './condiciones/condiciones.component';
 import {FaqComponent} from './faq/faq.component'
 import {CarritoComponent} from './carrito/carrito.component';
-import {FormularioContactosComponent} from './formulario-contactos/formulario-contactos.component'
-import {FormularioLoginComponent} from './formulario-login/formulario-login.component'
-import {FormularioRegistroComponent} from './formulario-registro/formulario-registro.component'
 export const routes: Routes = [
   {path: '', component:MenuComponent},
   {path: 'catalogo', component:CatalogoComponent},
   {path: 'condiciones', component:CondicionesComponent},
   {path: 'faq', component:FaqComponent},
   {path: 'carrito', component:CarritoComponent},
-  {path: 'formulario-contactos', component:FormularioContactosComponent},
-  {path: 'formulario-login', component:FormularioLoginComponent},
-  {path: 'formulario-registro', component:FormularioRegistroComponent},
+
 ];
