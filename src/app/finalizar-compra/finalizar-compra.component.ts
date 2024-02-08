b330927c047079c7be7da7ea594a67d28fbd2fea
eduar <eduardojosue.cerrato@institutvidreres.cat>
2024-02-08 11:35:33 +0100

    formulario carrito y componente

---
+++ src/app/finalizar-compra/finalizar-compra.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-finalizar-compra',
+  standalone: true,
+  imports: [],
+  templateUrl: './finalizar-compra.component.html',
+  styleUrl: './finalizar-compra.component.css'
+})
+export class FinalizarCompraComponent {
+
+}
