f2f4e1bc254a314abd7f0c70069b3f1e4844fb3c
eduar <eduardojosue.cerrato@institutvidreres.cat>
2024-02-15 08:47:13 +0100

    Prueba Firebase Nodejs

--- src/server.js
+++ src/server.js
@@ -1,10 +1,27 @@
 const express = require ('express')
 const app = express()
+const cors = require('cors');
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore} = require('firebase-admin/firestore');
+const serviceAccount = require('C:\\Users\\eduar\\IdeaProjects\\Proyecto-Angular_Cerrato_Nievas\\proyectolea-138e0-firebase-adminsdk-u5k4o-bcd6f093b9.json');
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
 
 app.use(express.json())
-
+app.use(cors())
 port = 3080
 
 app.listen(port, () => {
   console.log(`puerto de server::${port}`)
+})
+const tienda = db.collection('BlackDiamond').doc('User')
+app.get('/prueba', async(req, res) =>{
+  const docUser = await tienda.get();
+  const usuarios = docUser.data();
+  res.json (usuarios);
+  console.log(usuarios);
 })
