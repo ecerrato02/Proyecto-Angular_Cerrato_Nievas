24f0c31150131645ca8efbeaddcbea7b03f06124
eduar <eduardojosue.cerrato@institutvidreres.cat>
2024-03-19 21:55:27 +0100

    acabado

--- src/app/formulario-contactos/formulario-contactos.component.ts
+++ src/app/formulario-contactos/formulario-contactos.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { ElementRef, ViewChild } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 import {NgIf} from "@angular/common";
+import {RecaptchaModule} from "ng-recaptcha";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-formulario-contactos',
@@ -10,22 +11,49 @@ import {NgIf} from "@angular/common";
     RouterLink,
     RouterLinkActive,
     RouterOutlet,
-    NgIf
+    NgIf,
+    RecaptchaModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './formulario-contactos.component.html',
   styleUrl: './formulario-contactos.component.css'
 })
 export class FormularioContactosComponent {
-  constructor(private router: Router) {}
-  @ViewChild('submitButton') submitButton!: ElementRef;
-  @ViewChild('form') form!: ElementRef;
-
+  contactForm: FormGroup;
+  captchaCompleted: boolean = false;
   formularioEnviado = false;
 
+
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      nombre: '',
+      correo: '',
+      asunto: '',
+      descripcion: '',
+      recaptcha: '',
+    });
+
+  }
+
+  onCaptchaResolved(response: any) {
+    console.log('reCAPTCHA resolved with response:');
+    if (response) {
+      this.captchaCompleted = true;
+    }
+  }
+  submitForm(){
+    if(this.captchaCompleted){
+      this.enviarFormulario();
+    }
+  }
   enviarFormulario() {
     this.formularioEnviado = true;
     setTimeout(() => {
       this.formularioEnviado = false;
     }, 5000);
   }
+  protected readonly onclick = onclick;
 }
+
